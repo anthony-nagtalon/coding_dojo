cfbc89e3e51a2b2c7404abb48b49174b1c193722
Allen Anthony Nagtalon <ant.nagtalon22@gmail.com>
2021-05-24 12:41:57 -0700

    Integrated BankAccount with User class

--- _python/OOP/user.py
+++ _python/OOP/user.py
@@ -1,43 +1,3 @@
-class User:
-    def __init__(self, name, email):
-        self.name = name
-        self.email = email
-        self.account_balance = 0
-
-    def make_deposit(self, amount):
-        self.account_balance += amount
-        return self
-
-    def make_withdrawal(self, amount):
-        self.account_balance -= amount
-        return self
-
-    def display_user_balance(self):
-        print("User: {}, Balance: ${}".format(self.name, self.account_balance))
-
-    def transfer_money(self, recipient, amount):
-        self.account_balance -= amount
-        recipient.account_balance += amount
-        return self
-
-# Create 3 Instances of User
-user1 = User("Catherine", "kyasurin@gmail.com")
-user2 = User("Hye-lin", "exid_hyelin_seo@gmail.com")
-user3 = User("Angie", "anjou_san@gmail.com")
-
-# Make 3 deposits, 1 withdrawal
-user1.make_deposit(100).make_deposit(200).make_deposit(300).make_withdrawal(250).display_user_balance()
-
-# Make 2 deposits, 2 withdrawals
-user2.make_deposit(50).make_deposit(175).make_withdrawal(25).make_withdrawal(100).display_user_balance()
-
-# Make 1 deposit, 3 withdrawals
-user3.make_deposit(3000).make_withdrawal(250).make_withdrawal(200).make_withdrawal(150).display_user_balance()
-
-# Tranfer money from the first user to third user
-user1.transfer_money(user3, 100).display_user_balance()
-user3.display_user_balance()
-
 class BankAccount:
     def __init__(self, int_rate = 0.01, balance = 0): 
         self.int_rate = int_rate
@@ -45,27 +5,120 @@ class BankAccount:
     
     def deposit(self, amount):
         self.balance += amount
-        return self
 
     def withdraw(self, amount):
         if self.balance < amount:
             print("Insufficient funds: Charging a $5 fee")
             self.balance -= 5
+            return False
             # Note: Balance can go under 0 if charged with less than $5 in account
         else:
             self.balance -= amount
-        return self
+            return True
 
     def display_account_info(self):
-        print("Balance: ${}".format(self.balance))
+        return "Balance: ${}".format(self.balance)
 
     def yield_interest(self):
         if self.balance > 0:
             self.balance *= (self.int_rate + 1)
+
+class User:
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.account = [BankAccount(int_rate = 0.02, balance = 0)]
+
+    def create_account(self, int_rate = 0.01, balance = 0):
+        self.account.append(BankAccount(int_rate, balance))
         return self
 
-acct1 = BankAccount()
-acct2 = BankAccount()
+    def verify_account_num(self, accIndex):
+        if 0 <= accIndex < len(self.account):
+            return True
+        else:
+            return False
 
-acct1.deposit(500).deposit(50).deposit(5).withdraw(412).yield_interest().display_account_info()
-acct2.deposit(1000).deposit(500).withdraw(25).withdraw(75).withdraw(550).withdraw(125).yield_interest().display_account_info()
+    def make_deposit(self, amount, accIndex):
+        if self.verify_account_num(accIndex):
+            self.account[accIndex].deposit(amount)
+            return True
+        else:
+            print("Invalid Deposit: User {} does not have an acct #{}.".format(self.name, accIndex))
+            return False
+
+    def make_withdrawal(self, amount, accIndex):
+        if self.verify_account_num(accIndex):
+            if self.account[accIndex].withdraw(amount):
+                return True
+        else:
+            print("Invalid Withdrawal: User {} does not have an acct #{}.".format(self.name, accIndex))
+        return False
+
+    def display_user_balance(self):
+        print("User: {}".format(self.name))
+        for i in range(len(self.account)):
+            print("[{}] {}".format(i+1, self.account[i].display_account_info()))
+
+    def transfer_money(self, accIndex, recipient, recAccIndex, amount):
+        # Check if acc numbers are valid for each user
+        if self.verify_account_num(accIndex) and recipient.verify_account_num(recAccIndex):
+            # Check if withdrawal is successful before depositing money to recipient
+            if self.make_withdrawal(amount, accIndex):
+                recipient.make_deposit(amount, recAccIndex)
+        
+        # Display which acc numbers are invalid
+        else:
+            if not self.verify_account_num(accIndex):
+                print("Invalid Transaction: User {} does not have an acct #{}.".format(self.name, accIndex))
+            if not recipient.verify_account_num(recAccIndex):
+                print("Invalid Transaction: User {} does not have an acct #{}.".format(recipient.name, recAccIndex))
+        return self
+
+# Create 3 Instances of User
+user1 = User("Catherine", "kyasurin@gmail.com")
+user2 = User("Hye-lin", "exid_hyelin_seo@gmail.com")
+user3 = User("Angie", "anjou_san@gmail.com")
+
+# Return value changed from self to boolean for use in verifying transcations!
+# Therefore, we cannot utilize chaining methods.
+user1.create_account()
+user1.make_deposit(100, 0)
+user1.make_deposit(500, 1)
+user1.display_user_balance()
+print()
+
+# We can still chain with create_account because it returns self
+user2.create_account().create_account(0.15, 1000)
+user2.make_deposit(250, 0)
+user2.make_deposit(300, 1)
+user2.display_user_balance()
+print()
+
+# Example of invalid deposit
+user2.make_deposit(3, 3)
+print()
+
+# Example of invalid withdrawal
+user3.make_withdrawal(1, 1)
+print()
+
+# Example of withdrawal with insufficient balance
+user3.create_account(balance = 20).make_withdrawal(25, 1)
+print()
+
+# Example of successful transfer
+user1.transfer_money(1, user3, 1, 200)
+user1.display_user_balance()
+user3.display_user_balance()
+print()
+
+# Example of invalid transfer indices
+user1.transfer_money(2, user3, 2, 100)
+print()
+
+# Example of invalid transfer due to insufficient balance
+user3.transfer_money(0, user2, 0, 10)
+# Note: User3 will have a negative balance due to $5 fee
+user2.display_user_balance()
+user3.display_user_balance()
