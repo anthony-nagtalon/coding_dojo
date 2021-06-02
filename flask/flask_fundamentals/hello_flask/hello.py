c9eaa35968e5948d2a1714c55c4136acf35f3349
Allen Anthony Nagtalon <ant.nagtalon22@gmail.com>
2021-06-02 12:40:39 -0700

    Flask Fundamentals - Playground

--- flask/flask_fundamentals/hello_flask/hello.py
+++ flask/flask_fundamentals/hello_flask/hello.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html', phrase="hello", times=5)
 
 @app.route('/success')
 def success():
@@ -15,7 +15,7 @@ def hello(name):
     print(name)
     return "Hello, " + name
 
-@app.route('users/<username>/<id>')
+@app.route('/users/<username>/<id>')
 def show_user_profile(username, id):
     print(username)
     print(id)
