4a806342a16382d3abb65f501c613ee6490d2164
Allen Anthony Nagtalon <ant.nagtalon22@gmail.com>
2021-06-03 13:14:21 -0700

    Flask Fund. - HTML Table

--- flask/flask_fundamentals/hello_flask/hello.py
+++ flask/flask_fundamentals/hello_flask/hello.py
@@ -21,5 +21,15 @@ def show_user_profile(username, id):
     print(id)
     return "Username: " + username + ", id: " + id
 
+@app.route('/lists')
+def render_lists():
+    student_info = [
+       {'name' : 'Michael', 'age' : 35},
+       {'name' : 'John', 'age' : 30 },
+       {'name' : 'Mark', 'age' : 25},
+       {'name' : 'KB', 'age' : 27}
+    ]
+    return render_template("lists.html", random_numbers = [3,1,5], students = student_info)
+
 if __name__ == "__main__":
     app.run(debug = True)
