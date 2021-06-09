d1b523bc09d40cedc867b14f7f3e26b937bcd7bd
Allen Anthony Nagtalon <ant.nagtalon22@gmail.com>
2021-06-09 14:59:17 -0700

    Flask + MySQL - Query Example w/ Santization

--- flask/flask_with_mysql/first_flask_mysql/server.py
+++ flask/flask_with_mysql/first_flask_mysql/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from mysqlconnection import connectToMySQL
 
 app = Flask(__name__)
@@ -9,7 +9,23 @@ def index():
     friends = mysql.query_db("SELECT * FROM friends;")
 
     print(friends)
-    return render_template("index.html")
+    return render_template("index.html", all_friends = friends)
+
+@app.route("/create_friend", methods=["POST"])
+def add_friend_to_db():
+    print(request.form)
+
+    mysql = connectToMySQL("first_flask")
+
+    query = "INSERT INTO friends (first_name, last_name, occupation, created_at, updated_at) VALUES (%(fn)s, %(ln)s, %(occup)s, NOW(), NOW());"
+    data = {
+        "fn": request.form["fname"],
+        "ln": request.form["lname"],
+        "occup": request.form["occ"]
+    }
+
+    new_friend_id = mysql.query_db(query, data)
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run(debug=True)
