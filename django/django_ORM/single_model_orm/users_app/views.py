14f59134c3745b66299e97383209edbdae265124
Allen Anthony Nagtalon <ant.nagtalon22@gmail.com>
2021-06-27 19:11:07 -0700

    Django - User with Templates

--- django/django_ORM/single_model_orm/users_app/views.py
+++ django/django_ORM/single_model_orm/users_app/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from users_app.models import *
 
 # Create your views here.
+def index(request):
+    context = {
+        "users": User.objects.all()
+    }
+    return render(request, "index.html", context)
+
+def process(request):
+    first_name = request.POST['first-name']
+    last_name = request.POST['last-name']
+    email = request.POST['email']
+    age = int(request.POST['age'])
+
+    new_user = User.objects.create(first_name=first_name, last_name=last_name, email_address=email, age=age)
+
+    return redirect("/")
