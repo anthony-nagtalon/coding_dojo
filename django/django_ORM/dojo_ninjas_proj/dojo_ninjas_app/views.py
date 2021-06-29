5bfaf7fdb6273c23b51ce742143522769c43d3d1
Allen Anthony Nagtalon <ant.nagtalon22@gmail.com>
2021-06-29 13:43:14 -0700

    Dojo & Ninjas w/ Templates

--- django/django_ORM/dojo_ninjas_proj/dojo_ninjas_app/views.py
+++ django/django_ORM/dojo_ninjas_proj/dojo_ninjas_app/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from dojo_ninjas_app.models import *
 
 # Create your views here.
+def index(request):
+    context = {
+        "dojos": Dojo.objects.all(),
+        "ninjas": Ninja.objects.all()
+    }
+    return render(request, "index.html", context)
+
+def create_dojo(request):
+    Dojo.objects.create(name=request.POST['name'], city=request.POST['city'], state=request.POST['state'])
+    return redirect("/")
+
+def create_ninja(request):
+    Ninja.objects.create(dojo=Dojo.objects.get(id=request.POST['dojo-id']), 
+        first_name=request.POST['first-name'], 
+        last_name=request.POST['last-name'])
+    return redirect("/")
