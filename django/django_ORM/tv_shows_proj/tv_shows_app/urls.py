d936897177e7b74f089f5d0dc9bab1ed2dd53034
Allen Anthony Nagtalon <ant.nagtalon22@gmail.com>
2021-07-01 15:21:35 -0700

    Adjusted urls to match project prompt

--- django/django_ORM/tv_shows_proj/tv_shows_app/urls.py
+++ django/django_ORM/tv_shows_proj/tv_shows_app/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.shows),
     path('shows', views.shows),
     path('shows/new', views.add_show_page),
-    path('add_show', views.add_show),
+    path('shows/create', views.add_show),
     path('shows/<int:id>', views.display_show),
     path('shows/<int:id>/edit', views.edit_show_page),
-    path('shows/<int:id>/edit_show', views.edit_show),
+    path('shows/<int:id>/update', views.edit_show),
     path('shows/<int:id>/destroy', views.delete_show)
 ]
