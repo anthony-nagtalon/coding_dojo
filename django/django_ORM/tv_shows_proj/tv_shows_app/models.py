2da359d62ed739ab13855fcc4f7a2dafa2f82255
Allen Anthony Nagtalon <ant.nagtalon22@gmail.com>
2021-07-01 17:10:04 -0700

    Added form validations

--- django/django_ORM/tv_shows_proj/tv_shows_app/models.py
+++ django/django_ORM/tv_shows_proj/tv_shows_app/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class ShowManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        if len(postData['title']) < 2:
+            errors["title"] = "Title should be at least 2 characters!"
+        if len(postData['network']) < 3:
+            errors["network"] = "Network should be at least 3 characters!"
+        if len(postData['desc']) < 10 and len(postData['desc']) > 0:
+            errors["desc"] = "Description should be at least 10 characters!"
+        return errors
+
 class Show(models.Model):
     title = models.CharField(max_length=255)
     network = models.CharField(max_length=63)
@@ -8,3 +19,4 @@ class Show(models.Model):
     desc = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = ShowManager()
--- django/django_ORM/tv_shows_proj/tv_shows_app/views.py
+++ django/django_ORM/tv_shows_proj/tv_shows_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from tv_shows_app.models import *
 
 # Create your views here.
@@ -12,13 +13,20 @@ def add_show_page(request):
     return render(request, "add_show.html")
 
 def add_show(request):
-    new_show = Show.objects.create(
-        title=request.POST['title'],
-        network=request.POST['network'],
-        release_date=request.POST['release-date'],
-        desc=request.POST['desc']
-    )
-    return redirect("/shows/{}".format(new_show.id)) # Switch to new entry ID
+    errors = Show.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect("/shows/new")
+    else:
+        new_show = Show.objects.create(
+            title=request.POST['title'],
+            network=request.POST['network'],
+            release_date=request.POST['release-date'],
+            desc=request.POST['desc']
+        )
+        messages.success(request, "Show successfully added")
+        return redirect("/shows/{}".format(new_show.id))
 
 def display_show(request, id):
     context = {
@@ -33,13 +41,19 @@ def edit_show_page(request, id):
     return render(request, "edit_show.html", context)
 
 def edit_show(request, id):
-    show = Show.objects.get(id=id)
-    show.title = request.POST['title']
-    show.network = request.POST['network']
-    show.release_date = request.POST['release-date']
-    show.desc = request.POST['desc']
-    show.save()
-    return redirect("/shows/{}".format(id)) # Switch to new entry ID
+    errors = Show.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect("/shows/{}/edit".format(id))
+    else:
+        show = Show.objects.get(id=id)
+        show.title = request.POST['title']
+        show.network = request.POST['network']
+        show.release_date = request.POST['release-date']
+        show.desc = request.POST['desc']
+        show.save()
+        return redirect("/shows/{}".format(id))
 
 def delete_show(request, id):
     Show.objects.get(id=id).delete()
